f4962a0def35d612b15d6f67504fe69f76670a38
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-06-09 14:09:02 -0700

    Add package info page man

---
+++ R/multiDiff.R
@@ -0,0 +1,11 @@
+#' multiDiff: Internal functions I use
+#'
+#' to do... can I get a list of functions? Just document them!
+#'
+#' Should run:
+#' remotes::install_github("MatthieuStigler/multiDiff", upgrade = "never")
+#'
+#'
+#' @docType package
+#' @name multiDiff
+NULL
