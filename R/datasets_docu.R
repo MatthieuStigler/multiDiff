e3cac58fe0fad83b606788d6e1411d793a524106
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-04-18 21:33:50 -0700

    Add Gentkow dataset

---
+++ R/datasets_docu.R
@@ -0,0 +1,19 @@
+#' Dataset from Gentzkwow et al (2011)
+#'
+#' This is the replication dataset put by Reed (2019)
+#'
+#'
+#' @name GentzkowData
+#' @docType data
+#' @format A data-frame
+#' @keywords datasets
+#' @references Reed, W. Robert, 2019, "Replication Data for: "GUEST BLOG at TRN: EIR - Heterogeneity in Two-Way Fixed Effects Models"", https://doi.org/10.7910/DVN/EGMLQG, Harvard Dataverse, V2, UNF:6:r9g/aJRpVkv2rxw3F3J7QQ==
+#' @examples
+#' library(multiDiff)
+#' data(GentzkowData)
+#' GentzkowData
+#'
+#' ## The FE is
+"GentzkowData"
+
+
---
+++ data_raw/prep_export_datasets.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(usethis)
+
+#############################
+## Import
+#############################
+
+load("data_raw/GentzkowData.RData")
+
+
+#############################
+## Prepare
+#############################
+
+## Downloaded form Harvard Dataverse
+# Reed, W. Robert, 2019, "Replication Data for: "GUEST BLOG at TRN: EIR - Heterogeneity in Two-Way Fixed Effects Models"", https://doi.org/10.7910/DVN/EGMLQG, Harvard Dataverse, V2, UNF:6:r9g/aJRpVkv2rxw3F3J7QQ== [fileUNF]
+GentzkowData <-  table %>%
+  select(-matches("^st[0-9]+"), -matches("^styr[0-9]+"))
+# rename(state=st, state_year = styr)
+
+
+
+#############################
+## export
+#############################
+
+
+use_data(GentzkowData, overwrite = TRUE)
