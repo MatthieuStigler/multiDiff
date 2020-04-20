d877e3585be2358d6a8e84d84582dce269e62823
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-04-20 09:08:46 -0700

    Minor change to push new tag

--- data_raw/prep_export_datasets.R
+++ data_raw/prep_export_datasets.R
@@ -12,7 +12,7 @@ load("data_raw/GentzkowData.RData")
 ## Prepare
 #############################
 
-## Downloaded form Harvard Dataverse
+## Downloaded form Harvard Dataverse.
 # Reed, W. Robert, 2019, "Replication Data for: "GUEST BLOG at TRN: EIR - Heterogeneity in Two-Way Fixed Effects Models"", https://doi.org/10.7910/DVN/EGMLQG, Harvard Dataverse, V2, UNF:6:r9g/aJRpVkv2rxw3F3J7QQ== [fileUNF]
 GentzkowData <-  table %>%
   select(-matches("^st[0-9]+"), -matches("^styr[0-9]+"))
