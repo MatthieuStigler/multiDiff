dcf965dfbbffe2a20b7588d6144d961c9ea8e305
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2022-11-09 12:17:04 +0100

    Convert tests/DD_tests.R into testthat

---
+++ tests/testthat/test-DiffDiff_byY.R
@@ -0,0 +1,21 @@
+library(multiDiff)
+data <- sim_dat(N=100)
+
+## TEST standard
+DD(data=data)
+
+
+## TEST no year with variation
+data3 <- data |>
+  dplyr::mutate(tr = 0)
+test_that("Output from DD is same as from DD_manu_many", {
+  expect_warning(DD_out <- DD(data=data3),
+                 "No variation in treatment found!?")
+})
+
+
+## TEST some years without variation
+data2 <- data |>
+  dplyr::mutate(tr = dplyr::if_else(Time %in% c(1,2), 0L, tr))
+DD_out <- DD(data=data2)
+DD_out
