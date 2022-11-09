9a7a4d5f1225431f7635a0e43ed19bf087c69aba
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2022-11-09 14:46:00 +0100

    More info on package

--- R/multiDiff-package.R
+++ R/multiDiff-package.R
@@ -1,7 +1,8 @@
-#' multiDiff: multi-perio diff-and-diff
+#' multiDiff: multi-period diff-and-diff
 #'
 #' This R package implements the multi-period diff-diff as discussed in
-#' Imai and Kim (2019) and Chaisemartin and Hautfeuille (2019).
+#' Imai and Kim (2019) and Chaisemartin and Hautfeuille (2019),
+#' as well as the Gibbons et al decomposition, and simple event study function.
 #'
 #' Main functions are \code{\link{DD}}, and \code{\link{DiD_aggreg}}.
 #' Function \code{\link{sim_dat}} simulates a dataset for the example.
