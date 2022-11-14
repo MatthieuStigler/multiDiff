463d0f6cd520d18df773eeea34859b52357b4aa4
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2022-11-14 11:37:27 +0100

    small fix

--- tests/testthat/test-test_pre_trend.R
+++ tests/testthat/test-test_pre_trend.R
@@ -23,3 +23,26 @@ test_that("Function runs smoothly", {
   expect_error(suppressWarnings(mdd_test_pre_trend_event(mdd_dat = ES_omit5)),
                "arguments imply differing number of rows: 0, 1")
 })
+
+
+################################
+#'## Other names
+################################
+
+mdd_data_alter <- data |>
+  dplyr::rename(unit_id=unit, time_id=Time,
+                y_outcome=y, treat_var=tr) |>
+  mdd_data_format(y_var = "y_outcome",
+                  unit.index = "unit_id", time.index = "time_id",
+                  treat = "treat_var")
+
+test_that("Works with different variables names: _means", {
+  expect_equal(mdd_test_pre_trend_means(mdd_dat=mdd_data),
+               mdd_test_pre_trend_means(mdd_dat=mdd_data_alter))
+})
+
+test_that("Works with different variables names: _event", {
+  expect_equal(mdd_test_pre_trend_event(mdd_dat=mdd_data),
+               mdd_test_pre_trend_event(mdd_dat=mdd_data_alter))
+})
+
