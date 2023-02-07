d39fdf21b7b7d3d4d859fd1c1ccfba9f73daa759
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2023-02-07 17:57:25 +0100

    Add weight argument to grou means

--- R/mdd_group_means.R
+++ R/mdd_group_means.R
@@ -4,6 +4,7 @@
 #'
 #' @template param_mdd_dat
 #' @param conf.int Weather to compute confidence intervals for the means
+#' @param weights Optional column name of weights variable
 #' @examples
 #'  library(multiDiff)
 #'  dat_DiD_raw <- sim_dat_common()
@@ -15,7 +16,7 @@
 #'  mdd_group_means(dat_DiD, conf.int = TRUE)
 
 #' @export
-mdd_group_means <- function(mdd_dat, conf.int=FALSE) {
+mdd_group_means <- function(mdd_dat, conf.int=FALSE, weights=NULL) {
 
   if(!inherits(mdd_dat, "mdd_dat")) stop("Data should be formatted with 'mdd_data_format' first ")
   mdd_dat_slot <- intrnl_mdd_get_mdd_slot(mdd_dat)
@@ -26,16 +27,22 @@ mdd_group_means <- function(mdd_dat, conf.int=FALSE) {
     add_group(time.index = mdd_vars$time.index, treat = mdd_vars$treat,
               unit.index = mdd_vars$unit.index, group_rename_maybe=TRUE)
 
+  ## process wieghts argument
+  is_w_null <- rlang::quo_is_null(enquo(weights)) ## check if null
+  if(!is_w_null & !rlang::quo_is_symbol(rlang::enquo(weights))) weights <- rlang::sym(weights) # if not null and char, then to quo
+
   ## Compute means
   if(!conf.int) {
+    # mean_fo <- if(!is.null(weights)) \(x) weighted.mean(x, w={{weights}}) else mean
     res_means <- mdd_dat_add %>%
       group_by(dplyr::across(c(".group", mdd_vars$time.index))) %>%
-      summarise(dplyr::across(mdd_vars$y_var, mean),
+      summarise(dplyr::across(mdd_vars$y_var, if(!is_w_null) \(x) weighted.mean(x, w={{weights}}) else mean),
                 .groups = "drop") %>%
       arrange(.data$.group, mdd_vars$time.index)
 
   } else {
     formu <- as.formula(paste(mdd_vars$y_var, "~1"))
+    w_lm <- if(is_w_null) rlang::missing_arg() else rlang::as_name(rlang::enquo(weights))
     res_means <- mdd_dat_add %>%
       group_by(dplyr::across(c(".group", mdd_vars$time.index))) %>%
       nest() %>%
@@ -58,10 +65,17 @@ if(FALSE){
   library(multiDiff)
   dat_DiD_raw <- sim_dat_common()
   dat_DiD <- mdd_data_format(data=dat_DiD_raw)
+  dat_DiD$weightsss <-1
+  as_tibble(dat_DiD)
 
   ## to mdd
   environment(mdd_group_means) <- environment(mdd_event_study)
   mdd_group_means(dat_DiD)
-  mdd_group_means(dat_DiD, conf.int = TRUE)
+  mdd_group_means(dat_DiD, weights=weightsss)
+  mdd_group_means(dat_DiD, weights="weightsss")
+  all.equal(mdd_group_means(dat_DiD), mdd_group_means(dat_DiD, weights=weightsss))
+
+  all.equal(mdd_group_means(dat_DiD, conf.int = TRUE),
+            mdd_group_means(dat_DiD, weights=weightsss, conf.int = TRUE))
 
 }
--- tests/testthat/test-mdd_group_means.R
+++ tests/testthat/test-mdd_group_means.R
@@ -19,17 +19,41 @@ means_stag_CI <- mdd_group_means(dat_stag, conf.int = TRUE)
 length(unique(means_any_noCI$.group))
 length(unique(means_stag_noCI$.group))
 
-test_that("Same means for common", {
+test_that("Same means with/without CI for common", {
   expect_equal(means_noCI$y,
                means_CI$y)
 })
 
-test_that("Same means for any", {
+test_that("Same means with/without CI for any", {
   expect_equal(means_any_noCI$y,
                means_any_CI$y)
 })
 
-test_that("Same means for stag", {
+test_that("Same means with/without CI for stag", {
   expect_equal(means_stag_noCI$y,
                means_stag_CI$y)
+})
+
+
+### test weights
+dat_any$weightsss <-1
+dat_DiD$weightsss <-1
+dat_stag$weightsss <-1
+
+test_that("Same means with/without 1 weights", {
+  expect_equal(mdd_group_means(dat_any),
+               mdd_group_means(dat_any, weights=weightsss))
+  expect_equal(mdd_group_means(dat_DiD),
+               mdd_group_means(dat_DiD, weights=weightsss))
+  expect_equal(mdd_group_means(dat_stag),
+               mdd_group_means(dat_stag, weights=weightsss))
+})
+
+test_that("Same means with/without 1 weights, with CI", {
+  expect_equal(mdd_group_means(dat_any, conf.int = TRUE),
+               mdd_group_means(dat_any, weights=weightsss, conf.int = TRUE))
+  expect_equal(mdd_group_means(dat_DiD, conf.int = TRUE),
+               mdd_group_means(dat_DiD, weights=weightsss, conf.int = TRUE))
+  expect_equal(mdd_group_means(dat_stag, conf.int = TRUE),
+               mdd_group_means(dat_stag, weights=weightsss, conf.int = TRUE))
 })
