5004b1d84ae1a61a5036c86265ce46aecd453035
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2023-08-03 15:31:43 +0200

    add test for new coef and tidy

--- tests/testthat/test-mdd_synthdid.R
+++ tests/testthat/test-mdd_synthdid.R
@@ -19,10 +19,26 @@ head(W)
 res_rerun <- mdd_DD_simple(mdd_california_prop99, weights =  W$weights)
 
 ## Compare with feols_output
-test_that("feols_output=TRUE gives same output", {
+test_that("add_weights=TRUE gives same output", {
   expect_equal(res[1],
                coef(res_rerun)[[1]])
 })
+
+## check coef method
+test_that("Coef method works", {
+  expect_equal(coef(res),
+               -15.603828)
+})
+
+test_that("Tidy method works", {
+  expect_s3_class(tidy(res),
+                  "data.frame")
+})
+
+test_that("Tidy method works", {
+  expect_no_error(tidy(res, conf.int = TRUE))
+})
+
 
 ### means
 # res_full_dat <- attr(res_full, "mdd_data")
