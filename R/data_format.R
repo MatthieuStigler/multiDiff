427402762c56f320bbf6967a8b438427565bfc34
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2023-10-15 11:48:21 +0200

    Fix bug in printing mdd output

--- R/data_format.R
+++ R/data_format.R
@@ -78,8 +78,9 @@ print.mdd_dat <- function(x, ...){
 
   ## Printing of treated periods
   treated_periods_clean <- mdd_dat_slot$treated_periods
+  is_regular_seq <- length(unique(diff(treated_periods_clean)))==1
   expected_seq <- min(treated_periods_clean):max(treated_periods_clean)
-  if(all(treated_periods_clean== expected_seq)) {
+  if(is_regular_seq  && all(treated_periods_clean== expected_seq)) {
     treated_periods_print <- paste(range(treated_periods_clean), collapse = ":")
   } else {
     treated_periods_print <- treated_periods_clean
