87eb0c7da7186628c6d72c5bed8e0375ba212470
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2024-10-07 09:49:11 +0200

    gsynth: add tests

--- tests/testthat/test-mdd_gsynth.R
+++ tests/testthat/test-mdd_gsynth.R
@@ -45,3 +45,19 @@ test_that("tidy works with MC", {
   expect_no_error(tidy(x=res_mc, type ="time"))
   expect_no_error(tidy(x=res_mc, type ="average"))
 })
+
+
+test_that("Relationship with FE: with r=0 and force = 'two-way'", {
+  gs_FE2_only <- gsynth(Y ~ D, data = mdd_simdata_gs, parallel = FALSE,
+                         index = c("id","time"), force = "two-way", estimator="ife",
+                         CV = FALSE, r = 0, se = FALSE)
+  DD_simple <- mdd_DD_simple(mdd_simdata_gs)
+  DD_ES_post <- mdd_event_study(mdd_simdata_gs, trim_low = -1)
+
+  ## average is same as DiD
+  expect_equal(gs_FE2_only$att.avg,
+               coef(DD_simple)[[1]])
+  ## average is same as DiD
+  expect_equal(gs_FE2_only$att[21:30],
+               coef(DD_ES_post), ignore_attr="names")
+  })
