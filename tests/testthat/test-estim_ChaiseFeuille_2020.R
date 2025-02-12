df87d2c5e3555be4ea5fe2667b01cf9ac0e6760f
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2025-02-12 11:40:06 +0100

    Add a few tests, infos

--- tests/testthat/test-estim_ChaiseFeuille_2020.R
+++ tests/testthat/test-estim_ChaiseFeuille_2020.R
@@ -32,45 +32,46 @@ test_that("tidy works", {
 #'## Equalities
 ################################
 
-dat_common <- sim_dat_common(timing_treatment = 5:10, as_mdd = TRUE)
+dat_common <- sim_dat_common(timing_treatment = 5:10, as_mdd = TRUE, seed=123)
 
 mdd_DD <- mdd_DD_simple(dat_common)
 mdd_DD_cut <- mdd_DD_simple(dat_common |>subset(Time%in%c(4,5)))
 mdd_ES <- mdd_event_study(dat_common)
-mdd_CH_24_0 <- mdd_estim_CH(mdd_dat = dat_common)
-mdd_CH_24_dynamic <- mdd_estim_CH(mdd_dat = dat_common, effects =5, placebo=3)
+
+mdd_CH_24_effct_1 <- mdd_estim_CH(mdd_dat = dat_common)
+mdd_CH_24_effct_5 <- mdd_estim_CH(mdd_dat = dat_common, effects =5, placebo=3)
 
 
 
 ## pkg rounds coefficients! mat_res_XX[,1:4] <- round(mat_res_XX[,1:4],5)
-test_that("CH with no effects is same as 2-2 (but with tol!!)", {
+test_that("CH with effects=1 is same as 2-2 (but with tol!!)", {
   expect_equal(coef(mdd_DD_cut),
-               coef(mdd_CH_24_0),
+               coef(mdd_CH_24_effct_1),
                ignore_attr=TRUE,
                tolerance=1e-5)
 })
 
-test_that("CH with no effects is same as ES (but with tol!!)", {
+test_that("CH with effects=1 is same as first ES coef (but with tol!!)", {
   expect_equal(coef(mdd_ES)["timing_to_treat0"],
-               coef(mdd_CH_24_0),
+               coef(mdd_CH_24_effct_1),
                ignore_attr=TRUE,
                tolerance=1e-5)
 })
 
-test_that("CH with effects is same as ES (but with tol!!)", {
-  expect_equal(coef(mdd_CH_24_dynamic, type="Effects")[1:5],
-                 coef(mdd_ES, type="Effects")[4:8],
-                 tolerance=1e-5,
-                 ignore_attr=TRUE)
+test_that("CH with effects=5 is same as 5 first ES coefs (but with tol!!)", {
+  expect_equal(coef(mdd_CH_24_effct_5, type="Effects")[1:5],
+               coef(mdd_ES, type="Effects")[4:8],
+               tolerance=1e-5,
+               ignore_attr=TRUE)
 })
 
 test_that("ATE is just mean effects?", {
-  expect_equal(mean(coef(mdd_CH_24_dynamic, type="Effects")[1:5]),
-               coef(mdd_CH_24_dynamic, type="ATE"),
+  expect_equal(mean(coef(mdd_CH_24_effct_5, type="Effects")[1:5]),
+               coef(mdd_CH_24_effct_5, type="ATE"),
                tolerance=1e-5,
                ignore_attr=TRUE)
-  expect_equal(coef(mdd_CH_24_0, type="Effects"),
-               coef(mdd_CH_24_0, type="ATE"),
+  expect_equal(coef(mdd_CH_24_effct_1, type="Effects"),
+               coef(mdd_CH_24_effct_1, type="ATE"),
                tolerance=1e-5,
                ignore_attr=TRUE)
 })
